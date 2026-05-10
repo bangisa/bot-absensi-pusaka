4266a5d5f9ce1419ccb07ffcf5a004ad3ab95fd4
IsaOfficial <isamaulana2003@gmail.com>
2026-05-11 02:33:13 +0700

    version 1.0.0

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ app.use(
   }),
 );
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate",
+  );
+
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+
+  next();
+});
+
 app.use(
   session({
     secret: process.env.APP_SECRET || "secret123",
